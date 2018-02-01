6e5e9b1487535603c6faee3860444d24352d83c2
Eugene Spirkov <spirkov-work@bigmir.net>
2018-02-01 18:11:59 +0200

    lesson 7

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.multiple-items').slick({
+		autoplay: true,
+		autoplaySpeed: 2000,
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+	});
+})
